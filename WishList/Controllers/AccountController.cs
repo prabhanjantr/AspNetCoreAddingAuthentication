69fb56e7dfc51a11231e4aa0c3b5d78910ececb3
prabhanjan <prabhanjan.t.r@gmail.com>
2019-03-04 13:44:22 +0530

    Update AccountController.cs

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -42,7 +42,7 @@ namespace WishList.Controllers
                 }
                 return View(model);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index","Home");
         }
     }
 }
